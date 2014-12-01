7a2cca5e7a979f09e1e9774f737ce929ac4c4889
José Pinheiro Neta <jose.neta@gmail.com>
2014-12-01 10:05:06 +0000

    FIX formatting
    Signed-off-by: José Pinheiro Neta <jose.neta@gmail.com>

--- gistme.go
+++ gistme.go
@@ -1,26 +1,26 @@
 package gistme
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"encoding/json"
-  "strings"
+	"strings"
 )
 
 // FIXME -- 2014-11-30 12:33 UTC --JPN --
 // move the token out of here, for example keep it inside an ENV var.
 const (
-	TOKEN = "<PUT YOUR TOKEN HERE>"
+	TOKEN        = "<PUT YOUR TOKEN HERE>"
 	API_ENDPOINT = "https://api.github.com/gists"
 )
 
 type Gist struct {
 	//ua *http.Client
 	*http.Client
-  Description string                  `json:"description"`
-  Public bool                         `json:"public"`
-  Files map[string]map[string]string  `json:"files"`
+	Description string                       `json:"description"`
+	Public      bool                         `json:"public"`
+	Files       map[string]map[string]string `json:"files"`
 }
 
 //
@@ -34,7 +34,7 @@ func NewGist() Gist {
 }
 
 func (g Gist) All() string {
-	req, err := http.NewRequest("GET", API_ENDPOINT , nil)
+	req, err := http.NewRequest("GET", API_ENDPOINT, nil)
 	req.Header.Add("Basic", TOKEN+":x-oauth-basic")
 
 	resp, err := g.Do(req)
@@ -53,22 +53,22 @@ func (g Gist) All() string {
 // Create creates a new gist
 // TODO -- 2014-11-30 19:34 UTC --JPN --
 // add support for multiple files inside the gist
-// XXX Create only creates anonymous gists. However when you do a simple 
+// XXX Create only creates anonymous gists. However when you do a simple
 // curl -u <YOUR TOKEN HERE>:x-oauth-basic -XPOST https://api.github.com/gists -d@/tmp/data
-// the gist is as expected created in your account. So I'm probably doing 
+// the gist is as expected created in your account. So I'm probably doing
 // something wrong.
-func (g Gist) Create( name string, desc string, public bool, content string) string {
+func (g Gist) Create(name string, desc string, public bool, content string) string {
 	gist_alpha := Gist{
 		Description: desc,
 		Public:      true,
-		Files:       map[string]map[string]string{ name: map[string]string{ "content": content,},},
+		Files:       map[string]map[string]string{name: map[string]string{"content": content}},
 	}
 	b, err := json.Marshal(gist_alpha)
 	if err != nil {
 		log.Fatal("error:", err)
 	}
 
-	req, err := http.NewRequest("POST", API_ENDPOINT, strings.NewReader(string(b)) )
+	req, err := http.NewRequest("POST", API_ENDPOINT, strings.NewReader(string(b)))
 	req.Header.Add("Basic", TOKEN+":x-oauth-basic")
 
 	resp, err := g.Do(req)
